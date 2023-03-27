84900a75427e629a9e2e1d1d9488c742c6dde53b
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2023-03-27 16:36:50 -0300

    alterando configs

--- core/configs.py
+++ core/configs.py
@@ -1,13 +1,14 @@
 from typing import List
 from pydantic import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
 
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://lenner:mg1qD7C28e4xrr73qb6p2SZKC33xS4Va@dpg-cggu2kseoogqfc4kag10-a.ohio-postgres.render.com/faculdade'
+    DB_URL: str = 'postgresql+asyncpg://lenner:mg1qD7C28e4xrr73qb6p2SZKC33xS4Va@dpg-cggu2kseoogqfc4kag10-a/faculdade'
     DBBaseModel = declarative_base()
 
-    JWT_SECRET: str = 'QBseKsK3seN1mmZ-poqnXW3i0vedjcavdP8IDYtsPho'
+    JWT_SECRET: str = getenv(JWT_SECRET)
     ALGORITHM: str = 'HS256'
     ACESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
 
