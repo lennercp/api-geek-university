675d6befac9031aebb645a645be293bd3b0320f9
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2023-03-27 15:40:19 -0300

    alterado para render postgres

--- core/configs.py
+++ core/configs.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://postgres:123456@localhost:5432/faculdade'
+    DB_URL: str = 'postgresql+asyncpg://lenner:mg1qD7C28e4xrr73qb6p2SZKC33xS4Va@dpg-cggu2kseoogqfc4kag10-a.ohio-postgres.render.com/faculdade'
     DBBaseModel = declarative_base()
 
     JWT_SECRET: str = 'QBseKsK3seN1mmZ-poqnXW3i0vedjcavdP8IDYtsPho'
